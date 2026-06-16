6f2411c98e55e229ad8248359c007545640df340
amalnathrjwoxro <amal.nath@woxro.com>
2026-06-16 12:39:34 +0530

    fix: strip to minimal EB config

---
+++ server.js
@@ -0,0 +1,17 @@
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
+
+const port = parseInt(process.env.PORT || '8080', 10)
+const hostname = process.env.HOSTNAME || '0.0.0.0'
+const app = next({ dev: false, hostname, port })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  createServer(async (req, res) => {
+    const parsedUrl = parse(req.url, true)
+    await handle(req, res, parsedUrl)
+  }).listen(port, hostname, () => {
+    console.log(`> Ready on http://${hostname}:${port}`)
+  })
+})

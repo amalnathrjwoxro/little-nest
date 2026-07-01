ad7dba77b0605360fdd83982ffc09e594eb21f03
amalnathrjwoxro <amal.nath@woxro.com>
2026-07-01 09:57:30 +0530

    bug fixes

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './styles.css'
 import './globals.css'
 
+export const dynamic = 'force-dynamic'
+
 export const metadata = {
   description: 'A blank template using Payload in a Next.js app.',
   title: 'littlenest - Everything your little one needs',
